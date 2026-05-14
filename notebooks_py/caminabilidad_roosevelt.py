164f9ee232fcaaf70243773c9094f97101fe3520
Jorge <jorge@example.com>
2026-05-14 10:12:31 -0500

    update: script caminabilidad_roosevelt.py + CSV linea base

--- notebooks_py/caminabilidad_roosevelt.py
+++ notebooks_py/caminabilidad_roosevelt.py
@@ -7,7 +7,7 @@ Descarga la red peatonal y la guarda como GeoJSON en la carpeta de datos.
 
 Uso:
     uv run notebooks_py/caminabilidad_roosevelt.py
-    uv run notebooks_py/caminabilidad_roosevelt.py --geojson <ruta_poligono>
+   
 
 Repositorio: https://github.com/j0rg3c45/indice-caminabilidad-roosevelt.git
 """
